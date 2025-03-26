ca188b88121fdeeb8f079491ca363d9677b7af0c
felix <163010161+felixzhou9191@users.noreply.github.com>
2025-03-26 19:47:12 +0800

    浏览器从输入Url到页面渲染-Url的组成 (#5)
    * Url的组成
    * 添加算法专栏题目

--- .vitepress/sidebar.mjs
+++ .vitepress/sidebar.mjs
@@ -1,3 +1,11 @@
+/*
+ * @Author: felixzhou9191 felixzhou9191@gmail.com
+ * @Date: 2025-03-25 23:46:24
+ * @LastEditors: felixzhou9191 felixzhou9191@gmail.com
+ * @LastEditTime: 2025-03-25 23:55:35
+ * @FilePath: \YouZi_Stack\.vitepress\sidebar.mjs
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { generateSidebar } from 'vitepress-sidebar';
 
 export default generateSidebar([
@@ -42,5 +50,12 @@ export default generateSidebar([
         basePath: '/category/csstrick/',
         resolvePath: '/category/csstrick/',
         rootGroupText: 'CSS 揭秘'
+    },
+    {
+        documentRootPath: '/src',
+        scanStartPath: 'category/algorithm',
+        basePath: '/category/algorithm/',
+        resolvePath: '/category/algorithm/',
+        rootGroupText: '算法专栏'
     }
 ])
--- utils/_data.js
+++ utils/_data.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: felixzhou9191 felixzhou9191@gmail.com
+ * @Date: 2025-03-24 23:08:31
+ * @LastEditors: felixzhou9191 felixzhou9191@gmail.com
+ * @LastEditTime: 2025-03-24 23:44:53
+ * @FilePath: \YouZi_Stack\utils\_data.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 // This file is auto-generated. Do not edit manually.
 /** @type {import('vitepress').DefaultTheme.Config['sidebar']} */
 export const sidebarData = {
@@ -9,7 +17,15 @@ export const sidebarData = {
         {
           "text": "浏览器渲染原理01",
           "link": "/blog/浏览器渲染原理01"
-        }
+        },
+        {
+          "text": "浏览器渲染原理02",
+          "link": "/blog/浏览器渲染原理02"
+        },
+        {
+          "text": "从输入Url到页面渲染",
+          "link": "/blog/从输入Url到页面渲染"
+        },
       ]
     }
   ],
